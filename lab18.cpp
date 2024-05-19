6780d2a98e0df2a5c39f26e2a4993d775f563aa8
lilrain628 <147683935+lilrain628@users.noreply.github.com>
2024-05-19 22:48:25 +0700

    Update lab18.cpp

--- lab18.cpp
+++ lab18.cpp
@@ -1,63 +1,89 @@
 #include <iostream>
+#include <string>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
-// Определение класса "Места общественного питания"
-class Restaurant {
+class restaurant {
 public:
     string name;
     string type;
-    int rating;
-    string address;
+    string adress;
+
+public:
+    restaurant(string namerestaurant, string typerestaurant, string adressrestaurnt) {
+        name = namerestaurant;
+        type = typerestaurant;
+        adress = adressrestaurnt;
+    }
+
+    void outputTo() {
+        cout << "название завдения: " << name << endl;
+        cout << "Тип заведения: " << type << endl;
+        cout << "Адресс заведения: " << adress << endl;
+    }
+
+    string get_name() const {
+        return name;
+    }
+
+    string get_type() const {
+        return type;
+    }
+    string get_adress() const {
+        return adress;
+    }
+
+    void set_name(string f) {
+        name = f;
+    }
+
+    // Оператор для сортировки по  номеру группы
+    bool operator<(const restaurant& other) const {
+     return type < other.type;
+  }
 };
 
 int main() {
-    int originalSize, newSize;
+    // Создаем исходный вектор объектов ManagementOrganization
+    vector<restaurant> restaurant_list;
+    restaurant_list.push_back(restaurant("мельница", "бар", "Пушкина4"));
+    restaurant_list.push_back(restaurant("русь", "столовая", "koltushkina5"));
+    restaurant_list.push_back(restaurant("южане горожане ", "ресторан", "Ivanova6"));
+    restaurant_list.push_back(restaurant("додо пицца", "ресторан", "Petrova7"));
+    restaurant_list.push_back(restaurant("пивной причал", "бар", "Sidorov8"));
+    restaurant_list.push_back(restaurant("вкусно и точка", "ресторан", "rybkin9"));
+    restaurant_list.push_back(restaurant("в гостях у ярика", "ресторан", "Bblkovo10"));
 
-    // Ввод размерности исходного вектора с клавиатуры
-    cout << "Введите размерность исходного вектора: ";
-    cin >> originalSize;
-
-    // Создание и заполнение исходного вектора
-    vector<Restaurant> originalVector(originalSize);
-    for (int i = 0; i < originalSize; i++) {
-        cout << "Введите название учреждения: ";
-        cin >> originalVector[i].name;
-        cout << "Введите тип учреждения: ";
-        cin >> originalVector[i].type;
-        cout << "Введите рейтинг учреждения: ";
-        cin >> originalVector[i].rating;
-        cout << "Введите адрес: ";
-        cin >> originalVector[i].address;
-    }
-
-    // Ввод размерности нового вектора с клавиатуры
-    cout << "Введите размерность нового вектора: ";
-    cin >> newSize;
-
-    // Создание нового вектора и переписывание некоторых объектов из исходного вектора
-    vector<Restaurant> newVector(newSize);
-    for (int i = 0; i < newSize; i++) {
-        newVector[i] = originalVector[i % originalSize]; // Просто повторяем объекты из исходного вектора
-    }
-
-    // Проверка, не оказался ли новый вектор пустым
-    if (newSize == 0) {
-        cout << "Новый вектор пустой." << endl;
-    } else {
-        // Сортировка объектов в новом векторе по рейтингу (пример)
-        sort(newVector.begin(), newVector.end(), [](const Restaurant &a, const Restaurant &b) {
-            return a.rating < b.rating;
-        });
-
-        // Вывод отсортированного нового вектора
-        cout << "Отсортированный новый вектор:" << endl;
-        for (const auto &restaurant : newVector) {
-            cout << "название: " << restaurant.name <<endl << "Тип: " << restaurant.type << ", Рейтинг: " << restaurant.rating << ", Адрес: " << restaurant.address << endl;
+    // Создаем новый вектор и переписываем в него объекты с типом заведения "ресторан"
+    vector<restaurant> newRestaurant;
+    for ( int i=0;i<(int)restaurant_list.size();i++) {
+        if (restaurant_list[i].type== "ресторан") {
+            newRestaurant.push_back(restaurant_list[i]);
         }
     }
+
+    printf("\n");
+    for (int i=0;i<(int)newRestaurant.size();i++) {
+        newRestaurant[i].outputTo();
+        printf("\n");
+    }
+
+    // Проверяем, не оказался ли новый вектор пустым
+    if (newRestaurant.empty()) {
+        cout << "Новый вектор пуст." << endl;
+    } else {
+        // Сортируем объекты в новом векторе по номеру группы
+        sort(newRestaurant.begin(), newRestaurant.end());
+
+        // Выводим отсортированные объекты
+        cout << "Отсортированные объекты:" << endl;
+         printf("\n");
+        for (int i=0;i<(int)newRestaurant.size();i++) {
+            newRestaurant[i].outputTo();
+            printf("\n");
+        }
+    } 
 
     return 0;
 }
